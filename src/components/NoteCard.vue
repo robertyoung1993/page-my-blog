62255e45bf0e7b3aa3aaa55f26b1736e41e94d9c
robertyoung1993 <50591441+robertyoung1993@users.noreply.github.com>
2025-08-27 17:24:09 +0800

    Create NoteCard.vue

---
+++ src/components/NoteCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="note-card">
+    <p>{{ title }}</p>
+    <p>日期：{{ date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NoteCard',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.note-card {
+  background-color: #F9F4E9;
+  border-radius: 10px;
+  padding: 15px;
+  flex: 1;
+  transition: all 0.3s ease;
+}
+
+.note-card:hover {
+  border: 1px solid #E1BE97;
+}
+
+.note-card p:first-child {
+  font-size: 1.5vh;
+  color: black;
+  margin-bottom: 10px;
+}
+
+.note-card p:last-child {
+  font-size: 1vh;
+  color: gray;
+}
+</style>
