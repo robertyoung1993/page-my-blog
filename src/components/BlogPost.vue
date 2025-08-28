73a6d0f6cb22101646e16da9ccbeef389e8ffe4d
robertyoung1993 <50591441+robertyoung1993@users.noreply.github.com>
2025-08-28 19:41:10 +0800

    Update BlogPost.vue

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -3,7 +3,11 @@
     <img :src="image" :alt="title" class="blog-image">
     <div class="blog-content">
       <h3>{{ title }}</h3>
+      <p class="blog-meta">{{ date }} | {{ author }}</p>
       <p>{{ description }}</p>
+      <div class="blog-tags">
+        <span v-for="(tag, index) in tags" :key="index" class="tag">{{ tag }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -23,6 +27,18 @@ export default {
     description: {
       type: String,
       required: true
+    },
+    date: {
+      type: String,
+      default: ''
+    },
+    author: {
+      type: String,
+      default: ''
+    },
+    tags: {
+      type: Array,
+      default: () => []
     }
   }
 }
@@ -31,13 +47,12 @@ export default {
 <style scoped>
 .blog-item {
   display: flex;
-  margin-bottom: 30px;
   gap: 20px;
 }
 
 .blog-image {
-  width: 500px;
-  height: 120px;
+  width: 300px;
+  height: 150px;
   object-fit: cover;
   border-radius: 8px;
 }
@@ -49,11 +64,34 @@ export default {
 .blog-content h3 {
   font-size: 2.5vh;
   margin-bottom: 10px;
+  color: #333;
+}
+
+.blog-meta {
+  font-size: 1.3vh;
+  color: #777;
+  margin-bottom: 10px;
 }
 
 .blog-content p {
   font-size: 1.5vh;
   line-height: 1.6;
   color: #555;
+  margin-bottom: 15px;
+}
+
+/* 标签样式 */
+.blog-tags {
+  display: flex;
+  gap: 10px;
+  flex-wrap: wrap;
+}
+
+.tag {
+  background-color: #F9F4E9;
+  color: #664401;
+  padding: 5px 10px;
+  border-radius: 15px;
+  font-size: 1.2vh;
 }
 </style>
